feb4656e944b54f073a786ed3cb1376c667dec8d
zajc3w <zajc3w@gmail.com>
2013-06-27 00:22:42 +0100

    Update config.h

--- config.h
+++ config.h
@@ -3,21 +3,22 @@
 
 #include <avr/io.h>
 
-#define LCD_CONTROL_PORT PORTD
-#define LCD_CONTROL_PORT_DDR DDRD
-#define LCD_RS PD5
-#define LCD_RW PD4
+#define LCD_CONTROL_PORT PORTB
+#define LCD_CONTROL_PORT_DDR DDRB
+#define LCD_RS PB4
+#define LCD_RW PB7
+#define LCD_E PB6
 
-#define LCD_DATA_PORT PORTA
-#define LCD_DATA_PORT_PINS PINA
-#define LCD_DATA_PORT_DDR DDRA
-#define LCD_DB0 PA0
-#define LCD_DB1 PA1
-#define LCD_DB2 PA2
-#define LCD_DB3 PA3
-#define LCD_DB4 PA4
-#define LCD_DB5 PA5
-#define LCD_DB6 PA6
-#define LCD_DB7 PA7
+#define LCD_DATA_PORT PORTD  
+#define LCD_DATA_PORT_PINS PIND
+#define LCD_DATA_PORT_DDR DDRD
+#define LCD_DB0 PD0
+#define LCD_DB1 PD1
+#define LCD_DB2 PD2
+#define LCD_DB3 PD3
+#define LCD_DB4 PD4
+#define LCD_DB5 PD5
+#define LCD_DB6 PD6
+#define LCD_DB7 PD7
 
 #endif
